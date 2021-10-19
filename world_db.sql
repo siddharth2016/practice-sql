0ae8800d17b8f632a5808662fb465d8afcc513fe
siddharth2016 <siddharthchandragzb@gmail.com>
2021-10-19 15:27:56 +0530

    update world

--- world_db.sql
+++ world_db.sql
@@ -3,3 +3,9 @@ SELECT count(countrycode) FROM public.countrylanguage;
 SELECT avg(lifeexpectancy) FROM public.country;
 
 SELECT avg(population) FROM public.city WHERE countrycode = 'NLD';
+
+SELECT count(id) FROM city
+WHERE district IN ('Zuid-Holland', 'Noord-Brabant', 'Utrecht')
+
+SELECT DISTINCT lifeexpectancy FROM country
+WHERE lifeexpectancy IS NOT NULL

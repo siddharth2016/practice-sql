c1fb949ee7a2438b95ef385845e7e38ac488c868
siddharth2016 <siddharthchandragzb@gmail.com>
2021-10-23 17:34:19 +0530

    update france

--- france_db.sql
+++ france_db.sql
@@ -1,1 +1,11 @@
 SELECT count(id) FROM public.towns
+
+SELECT DISTINCT r.id,
+r.name,
+count(t.id) OVER (
+    PARTITION BY r.id
+)
+FROM regions AS r
+INNER JOIN departments AS d ON r.code = d.region
+INNER JOIN towns AS t ON d.code = t.department
+ORDER BY r.name

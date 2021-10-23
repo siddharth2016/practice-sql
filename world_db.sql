408e17a9d3796653fca049e8484d1722006b8762
siddharth2016 <siddharthchandragzb@gmail.com>
2021-10-23 17:35:03 +0530

    update world

--- world_db.sql
+++ world_db.sql
@@ -9,3 +9,19 @@ WHERE district IN ('Zuid-Holland', 'Noord-Brabant', 'Utrecht')
 
 SELECT DISTINCT lifeexpectancy FROM country
 WHERE lifeexpectancy IS NOT NULL
+
+SELECT DISTINCT continent,
+sum(population) OVER (
+    PARTITION BY continent
+)
+FROM country
+
+SELECT
+  DISTINCT continent,
+  SUM(population) OVER w1 AS"continent population"
+FROM country 
+WINDOW w1 AS( PARTITION BY continent );
+
+SELECT DISTINCT continent,
+round((sum(population::float4) OVER (PARTITION BY continent) / sum(population::float4) OVER()) * 100)
+FROM country
